70196dabe8c70424eb1e849b1502c1c6ad07c18c
Przemysław Rogala <74655029+Przemrog@users.noreply.github.com>
2023-10-24 01:10:31 +0200

    Update zad2.pl

--- zad2.pl
+++ zad2.pl
@@ -2,10 +2,10 @@ osoba(jan, 22, sport).
 osoba(anna, 33, kino).
 osoba(tomasz, 20, muzyka).
 osoba(marta, 23, czytanie).
-osoba(piotr, 33, podró¿e).
+osoba(piotr, 33, podroze).
 osoba(katarzyna, 65, gotowanie).
 
 
 starsza(X, Y) :- osoba(X, WiekX, _), osoba(Y, WiekY, _), WiekX > WiekY.
 mlodsza(X, Y) :- osoba(X, WiekX, _), osoba(Y, WiekY, _), WiekX < WiekY.
-równy_wiek(X, Y) :- osoba(X, WiekX, _), osoba(Y, WiekY, _), WiekX =:= WiekY.
+rÃ³wny_wiek(X, Y) :- osoba(X, WiekX, _), osoba(Y, WiekY, _), WiekX =:= WiekY.
